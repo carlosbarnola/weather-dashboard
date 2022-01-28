dc9a46be0bad21d2a48283994b952f0290e188b2
Carlos Rojas <carlosrb206@gmail.com>
2022-01-28 00:12:07 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchFormEl = document.querySelector('#search-form')
+const locationInputEl =document.querySelector('#location')
+const reccentlySearchEl = document.querySelector('#recently-search')
+const weatherSearchTerm = document.querySelector("#weather-search-term");
+const weatherContainerEl = document.querySelector("#weather-list");
+const weatherDayDiv = document.querySelector("#weather-day");
